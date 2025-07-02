94edac296faf69b7a2980e3ad7a3da06a6e66be2
cookies2002 <cookies2002boy@gmail.com>
2025-07-02 16:28:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-API_ID = 123456  # replace with your API_ID from my.telegram.org
-API_HASH = "your_api_hash_here"  # replace with your API_HASH
-SESSION = "madara_gf"  # name of your session file
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION = os.getenv("SESSION")
